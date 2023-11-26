f354c4f747d81d022aaae9d15cf72d798535909c
Thanaboon Burapanakan <telephonebura@gmail.com>
2023-11-27 00:56:53 +0700

    test

--- Sources/FaceLiveness/Views/Liveness/FaceLivenessDetectionViewModel.swift
+++ Sources/FaceLiveness/Views/Liveness/FaceLivenessDetectionViewModel.swift
@@ -150,7 +150,7 @@ class FaceLivenessDetectionViewModel: ObservableObject {
 
         let scaleRatio = cameraViewRect.width / videoSize.width
         let rect = CGRect(
-            x: 20,
+            x: 200,
             y: 140,
             width: ovalParameters.boundingBox.width / 2,
             height: ovalParameters.boundingBox.height / 2
