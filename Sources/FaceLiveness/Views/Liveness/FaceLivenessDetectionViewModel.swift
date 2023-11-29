c2dac4996260722e5f437a95a61675c7329bd723
Thanaboon Burapanakan <telephonebura@gmail.com>
2023-11-29 18:41:05 +0700

    test

--- Sources/FaceLiveness/Views/Liveness/FaceLivenessDetectionViewModel.swift
+++ Sources/FaceLiveness/Views/Liveness/FaceLivenessDetectionViewModel.swift
@@ -171,19 +171,19 @@ class FaceLivenessDetectionViewModel: ObservableObject {
             height: ovalHeightMid
         )
 
-        let normalizedOvalRect = CGRect(
-            x: rect.minX * scaleRatio,
-            y: rect.minY * scaleRatio,
-            width: rect.width * scaleRatio,
-            height: rect.height * scaleRatio
-        )
+        // let normalizedOvalRect = CGRect(
+        //     x: rect.minX * scaleRatio,
+        //     y: rect.minY * scaleRatio,
+        //     width: rect.width * scaleRatio,
+        //     height: rect.height * scaleRatio
+        // )
 
-        livenessViewControllerDelegate?.drawOvalInCanvas(normalizedOvalRect)
+        livenessViewControllerDelegate?.drawOvalInCanvas(rect)
         DispatchQueue.main.async {
             self.livenessState.ovalDisplayed()
             onComplete()
         }
-        ovalRect = normalizedOvalRect
+        ovalRect = rect
     }
 
 
