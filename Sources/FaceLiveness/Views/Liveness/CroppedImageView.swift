984582e065a65a8c0373f8af9d741a6a69cb3bdb
Thanaboon Burapanakan <telephonebura@gmail.com>
2023-12-07 14:09:17 +0700

    test

--- Sources/FaceLiveness/Views/Liveness/CroppedImageView.swift
+++ Sources/FaceLiveness/Views/Liveness/CroppedImageView.swift
@@ -12,8 +12,6 @@ class CroppedImageView: UIImageView {
     let scale = shorterSide / image.size.width
     
     contentMode = .scaleAspectFill
-    layer.contentsRect = CGRect(x: 0, y: 0, width: 1.0, height: 1.0)
     layer.setAffineTransform(CGAffineTransform(scaleX: scale, y: scale))
-    layer.position = CGPoint(x: frame.width / 2, y: frame.height / 2)
   }
 }
