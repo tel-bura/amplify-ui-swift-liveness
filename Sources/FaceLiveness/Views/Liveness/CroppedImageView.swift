a2b68ddc036fc9b922fa101727a0c24f9398210d
Thanaboon Burapanakan <telephonebura@gmail.com>
2023-12-07 14:03:34 +0700

    test

--- Sources/FaceLiveness/Views/Liveness/CroppedImageView.swift
+++ Sources/FaceLiveness/Views/Liveness/CroppedImageView.swift
@@ -6,15 +6,14 @@ class CroppedImageView: UIImageView {
   override func layoutSubviews() {
     super.layoutSubviews()
     
-    let radius = min(frame.width, frame.height) / 2
-    let path = UIBezierPath(arcCenter: CGPoint(x: frame.width / 2, y: frame.height / 2),
-                             radius: radius,
-                             startAngle: 0,
-                             endAngle: CGFloat(2 * Double.pi),
-                             clockwise: true)
+    guard let image = image else { return }
     
-    let maskLayer = CAShapeLayer()
-    maskLayer.path = path.cgPath
-    layer.mask = maskLayer
+    let shorterSide = min(frame.width, frame.height)
+    let scale = shorterSide / image.size.width
+    
+    contentMode = .scaleAspectFill
+    layer.contentsRect = CGRect(x: 0, y: 0, width: 1.0, height: 1.0)
+    layer.affineTransform = CGAffineTransform(scaleX: scale, y: scale)
+    layer.position = CGPoint(x: frame.width / 2, y: frame.height / 2)
   }
 }
