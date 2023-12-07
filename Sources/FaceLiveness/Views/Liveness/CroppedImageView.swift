d5835faf57b5348e9560fcd2c2b955e13c58702d
Thanaboon Burapanakan <telephonebura@gmail.com>
2023-12-07 14:11:26 +0700

    test

--- Sources/FaceLiveness/Views/Liveness/CroppedImageView.swift
+++ Sources/FaceLiveness/Views/Liveness/CroppedImageView.swift
@@ -4,14 +4,7 @@ import UIKit
 
 class CroppedImageView: UIImageView {
   override func layoutSubviews() {
-    super.layoutSubviews()
-    
-    guard let image = image else { return }
-    
-    let shorterSide = min(frame.width, frame.height)
-    let scale = shorterSide / image.size.width
-    
+    super.layoutSubviews()    
     contentMode = .scaleAspectFill
-    layer.setAffineTransform(CGAffineTransform(scaleX: scale, y: scale))
   }
 }
